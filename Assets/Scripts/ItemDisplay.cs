9faa4cd2be4eab17c21839339b2a1c42ebca79ca
jalper1 <alperjordan@gmail.com>
2024-02-25 00:56:35 -0600

    added scriptable object for item

---
+++ Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDisplay : MonoBehaviour
+{
+    public Items item;
+    public Text nameText;
+    public Text descriptionText;
+    public Image iconImage;
+    public Text attackPowerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = item.itemName;
+        descriptionText.text = item.itemDescription;
+        iconImage.sprite = item.itemIcon;
+        attackPowerText.text = item.attackPower.ToString();
+    }
+}
